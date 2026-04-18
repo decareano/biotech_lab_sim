088f5624667486d732a4baeb135f9a399cfd5f90
Marcelo Gobelli <marcelogobelli@Marcelos-MacBook-Pro-4.local>
2026-04-18 09:33:07 -0300

    testing scenarios 30 and 26 with the correct code after getting the errors

--- biotech_sim.py
+++ biotech_sim.py
@@ -3,7 +3,7 @@ from moto import mock_aws
 import json
 
 # --- THE MOCK LIMS DATABASE ---
-lims_db = {"EXP-402": {"status": "IDLE", "version": 2}}
+lims_db = {"EXP-402": {"status": "IDLE", "version": 1}}
 
 
 @mock_aws
@@ -16,7 +16,7 @@ def run_biotech_simulation():
     benchling_payload = {
         "entity": {
             "id": "EXP-402",
-            "version": 1,  # <--- ADD THIS LINE
+            # "version": 1,  # <--- ADD THIS LINE
             "fields": {"Mass Spec Data": {"value": "m/z: 450, intensity: 1200"}},
         }
     }
